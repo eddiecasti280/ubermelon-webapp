b9daad74a627aa9a9fd6fb46e5b998447766052f
Eddie Castillo <46185445+eddiecasti280@users.noreply.github.com>
2026-03-19 05:20:10 -0500

    File change

--- app.py
+++ app.py
@@ -301,5 +301,5 @@ def forbidden(e):
 # ── Entry Point ─────────────────────────────────────────────────────
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 5001))
     app.run(debug=True, port=port, host='0.0.0.0')
