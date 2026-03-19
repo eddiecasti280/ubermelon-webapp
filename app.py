917914951a599e4468def9382eb9c6dc40c1be25
Eddie Castillo <46185445+eddiecasti280@users.noreply.github.com>
2026-03-19 01:21:52 -0500

    Added files

--- app.py
+++ app.py
@@ -222,5 +222,5 @@ def page_not_found(e):
 
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 5001))
     app.run(debug=True, port=port, host='0.0.0.0')
